ef17d7ec10f07f20405b4d1b03ad794e6aedf6e5
solkri333 <160307302+solkri333@users.noreply.github.com>
2025-01-29 19:15:22 +0530

    Updated API url to Cloud Run endpoint

--- src/drivers.jsx
+++ src/drivers.jsx
@@ -20,7 +20,7 @@ function Drivers(props) {
         if (!props.season) return;
         const fetchData = async () => {
             try {
-                const response = await fetch('https://f1app-clex.onrender.com/ranking', {
+                const response = await fetch('https://f1-721508493198.us-central1.run.app/ranking', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json', // Specify JSON format
